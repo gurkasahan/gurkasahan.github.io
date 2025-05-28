a09440ae666943764dbe624e264bcdb205e62982
gurkasahan <gurk.asahan@gmail.com>
2025-05-28 00:14:00 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Heading, Text, VStack, Link } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={10} maxW="800px" mx="auto">
+      <VStack spacing={10} align="start">
+
+        <Box>
+          <Heading size="2xl">Gurk Asahan</Heading>
+          <Text fontSize="lg" color="gray.600">
+            Software Engineer · Data Analyst · Product Enthusiast
+          </Text>
+        </Box>
+
+        <Box>
+          <Heading size="lg">Projects</Heading>
+          <VStack align="start" mt={4} spacing={3}>
+            <Box>
+              <Text fontWeight="bold">Predictive Maintenance ML App</Text>
+              <Link color="blue.500" href="https://github.com/gurkasahan/predictive-maintenance-ml" isExternal>
+                GitHub Repo
+              </Link>
+            </Box>
+            <Box>
+              <Text fontWeight="bold">Parkinson’s Detection ML Model</Text>
+              <Link color="blue.500" href="https://github.com/gurkasahan/MachineLearning-Parkinson-Detection" isExternal>
+                GitHub Repo
+              </Link>
+            </Box>
+            <Box>
+              <Text fontWeight="bold">Global Life Expectancy Analysis</Text>
+              <Link color="blue.500" href="https://github.com/gurkasahan/global-life-expectancy-analysis" isExternal>
+                GitHub Repo
+              </Link>
+            </Box>
+          </VStack>
+        </Box>
+
+        <Box>
+          <Heading size="lg">Contact</Heading>
+          <Text>Email: gurk.asahan@gmail.com</Text>
+          <Text>
+            GitHub: <Link href="https://github.com/gurkasahan" isExternal color="blue.500">gurkasahan</Link>
+          </Text>
+          <Text>
+            LinkedIn: <Link href="https://linkedin.com/in/gurkasahan" isExternal color="blue.500">gurkasahan</Link>
+          </Text>
+        </Box>
+
+      </VStack>
+    </Box>
   );
 }
 
